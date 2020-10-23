a97bfef2f0d9afe37d0065c7e05d07028ca78fc1
padillal <padillal48@yahoo.com>
2020-10-23 01:27:00 -0700

    finished logic for game

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const canvas = document.getElementById('canvas');
 
 //ctx will allow for mods on the canvas
 const ctx = canvas.getContext('2d');
-const gridUnit = 25;
-const gridSize = 23;
+const gridUnit = 19;
+const gridSize = 20;
+let gridmod = (gridSize-1)*gridUnit;
 //score for the points
 let score = 0;
 //creating the snake array
@@ -21,19 +22,19 @@ document.addEventListener('keydown',direction);
 //creating input function
 function direction(input){
   //move left
-  if(input.keyCode == 37){
+  if(input.keyCode == 37 && movment != 'RIGHT'){
     movment = "LEFT";
   }
   //move up
-  if(input.keyCode == 38){
+  if(input.keyCode == 38 && movment != 'DOWN'){
     movment = "UP";
   }
   //move right
-  if(input.keyCode == 39){
+  if(input.keyCode == 39 && movment != 'LEFT'){
     movment = "RIGHT";
   }
   //move down
-  if (input.keyCode == 40){
+  if (input.keyCode == 40 && movment != 'UP'){
     movment = "DOWN";
   }
 }
@@ -47,44 +48,65 @@ let food = {
 //creating the game elements
 function draw(){
   //background color
-  ctx.fillStyle = 'cyan';
+  ctx.fillStyle = 'black';
   //initializing the postion and the size of the canvas minus boarder
   ctx.fillRect(gridUnit,gridUnit,gridSize*gridUnit-gridUnit,gridSize*gridUnit-gridUnit);
   //drawing the snake when the length increases
   for(let i=0; i<snake.length;i++){
-    ctx.fillStyle='green';
+    ctx.fillStyle='yellow';
     //initializing the position for the snake
-    ctx.fillRect(snake[0].x, snake[0].y, gridUnit,gridUnit);
+    ctx.fillRect(snake[i].x, snake[i].y, gridUnit,gridUnit);
   }
   //move the snake
-  let head = snake[0].x;
-  let tail = snake[0].y;
+  let pointX = snake[0].x;
+  let pointY = snake[0].y;
 
   if (movment == "LEFT")
-    snake[0].x -= gridUnit;
+    pointX -= gridUnit;
 
   if (movment == "UP")
-    snake[0].y -= gridUnit;
+    pointY -= gridUnit;
 
   if (movment == "RIGHT")
-    snake[0].x += gridUnit;
+    pointX += gridUnit;
 
   if (movment == "DOWN")
-    snake[0].y += gridUnit;
+    pointY += gridUnit;
 
   //checks to see when the food was eaten
-  if(snake[0].x == food.x && snake[0].y == food.y)
+  if(pointX == food.x && pointY == food.y)
   {
     score += 1;
     food = {
       x:Math.floor(1+(Math.random()*(gridSize-1))) * gridUnit,
       y:Math.floor(1+(Math.random()*(gridSize-1))) * gridUnit
     }
+  }else {
+    snake.pop();
   }
 
+  document.querySelector("#score").innerText = score;
+  //updating the size of the snake
+  let updateSnake = {x:pointX,y:pointY};
+
+  function collision(head,array){
+    for(let i = 0; i <array.length; i++){
+      if (head.x == array[i].x && head.y == array[i].y){
+        return true;
+      }
+    }
+    return false;
+  }
+  if (pointX < gridUnit || pointY < gridUnit ||
+        pointX > gridmod || pointY > gridmod || collision(updateSnake, snake)) {
+          clearInterval(game);
+
+  }
+   snake.unshift(updateSnake);
   //creates the icon for the food and the location
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = 'white';
   ctx.fillRect(food.x,food.y,gridUnit, gridUnit);
+
 }
 
 
